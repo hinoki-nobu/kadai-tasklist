5acf159890704016adb4afd996d0ea314419cb3a
nobu.yanagi <yangciliusheng2@gmail>
2023-03-26 12:22:20 +0000

    login kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+#  before_action :require_user_logged_in
+#  before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -10,15 +12,16 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
+#    @task = current_user.tasks.build(task_params)  
   end
 
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = "Task が正常に投稿されました。"
       redirect_to @task
     else
+#    @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))      
       flash.now[:danger] = "Task が投稿されませんでした。"
       render :new
     end
@@ -42,6 +45,7 @@ class TasksController < ApplicationController
     
     flash[:success] = "Task は正常に削除されました。"
     redirect_to tasks_url
+#    redirect_back(fallback_location: root_path)
   end
   
   private
@@ -54,4 +58,12 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "toppages#index"  
-
+  #root to: "toppages#index"
+  root to: "tasks#index"
+  
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   
-  get "signup", to: "users#new"
+  get "signup", to: "users#new"  
+
   resources :users, only: [:index, :show, :create]
+  resources :tasks
   
-  #resources :tasks
+#---------------------------------------
+#  resources :tasks, only: [:create, :destroy]
+#---------------------------------------  
 end
